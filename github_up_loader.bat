61faf5a45f0d37d9135f0c81036f22eb846eea06
Conmeosoma <dinhhoat157@gmail.com>
2025-07-20 09:54:21 +0700

    Update code

--- github_up_loader.bat
+++ github_up_loader.bat
@@ -1,19 +1,29 @@
 @echo off
 cd /d C:\Users\ADMIN\OneDrive\Desktop\OOP_JAVA
 
+:: Hiển thị con mèo
+echo.
+echo          /^^_^^\  
+echo         ( o.o ) 
+echo.
+echo     == MeoConSoMa ==
+echo.
+
+:: Các lệnh git
 echo ------------------------------------
-echo Dang them tat ca cac thay doi...
+echo [1/3] Dang them tat ca cac thay doi...
 git add .
 
-echo ------------------------------------
-echo Commit thay doi...
-git commit -m "Update code" || echo Khong co thay doi nao de commit
+echo ----------------------------------------
+echo [2/3] Commit thay doi...
+git commit -m "Update code" || echo => Khong co thay doi nao de commit.
 
-echo ------------------------------------
-echo Dang day code len GitHub...
+echo ----------------------------------------
+echo [3/3] Dang day code len GitHub...
 git push origin main --force
 
-echo ------------------------------------
-echo Done ! Da day code len github.
-echo Code by Meoconsoma.
+echo ----------------------------------------
+echo Done! Code da duoc day len GitHub.
+echo Code by MeoConSoMa.
+echo ----------------------------------------
 pause
