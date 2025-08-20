5d6506d2864a28d1b7ece1a09aef95fa767c48b8
Conmeosoma <tho.nam.tien@example.com>
2025-08-20 23:34:38 +0700

    Update code

--- Tuan7_BaiTapThem/src/Bai1/Main.java
+++ Tuan7_BaiTapThem/src/Bai1/Main.java
@@ -1,39 +1,35 @@
-// CodeByConMeoSoMa
-// /\_/\  
-//( o.o ) 
-// > ^ <
 package Bai1;
 
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int demHoa = 0;
+        int demAm = 0;
         System.out.print("Nhap: ");
         String s = sc.nextLine();
 
-        int demHoa = 0;
-        int demAm = 0;
+        char[] nguyenAm = {'a', 'e', 'i', 'o', 'u'};
 
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+            char kitu = s.charAt(i);
+            char check = Character.toLowerCase(kitu);
 
-            if (String.valueOf(ch).equalsIgnoreCase("a")
-                    || String.valueOf(ch).equalsIgnoreCase("e")
-                    || String.valueOf(ch).equalsIgnoreCase("i")
-                    || String.valueOf(ch).equalsIgnoreCase("o")
-                    || String.valueOf(ch).equalsIgnoreCase("u")) {
-                demAm++;
+            for (int j = 0; j < nguyenAm.length; j++) {
+                if (check == nguyenAm[j]) {
+                    demAm++;
+                }
             }
 
-            if (Character.isUpperCase(ch)) {
+            if (kitu >= 'A' && kitu <= 'Z') {
                 demHoa++;
             }
         }
 
-        System.out.println("So ki tu nguyen am " + demAm);
+        System.out.println("So ki tu nguyen am: " + demAm);
         System.out.println("So ki tu hoa: " + demHoa);
     }
 }
